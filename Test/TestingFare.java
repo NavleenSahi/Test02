c8dc1234e6a035b0c13764c691755b600d438e65
navleensahi <navleenk1@gmail.com>
2019-05-27 12:21:30 -0400

    R4- RED

--- Test/TestingFare.java
+++ Test/TestingFare.java
@@ -8,6 +8,8 @@ class TestingFare {
 	
 	Fare f = new Fare();
 
+	
+	//R1
 	@Test
 	void test() {
 		
@@ -18,6 +20,8 @@ class TestingFare {
 		assertEquals(2.5,fare,0.0);
 	}
 	
+	
+	//R2
 	@Test
 	void test1() {
 		String[] from1 = new String[] {"Sheppard"};
@@ -27,13 +31,27 @@ class TestingFare {
 		assertEquals(3.0,fare,0.0);
 	}
 	
+	
+	//R3
 	@Test
 	void test2() {
 		String[] from1 = new String[] {"Don Mills"};
 		String[] to1 = new String[] {"Finch Station"};
 		
-		Double fare =f.calculateTotal(from1,to1);
+		Double fare = f.calculateTotal(from1,to1);
 		assertEquals(3.0,fare,0.0);
 	}
-
+	
+	//R4
+		@Test
+		void test3() {
+			String[] from1 = new String[] {"Don Mills"};
+			String[] to1 = new String[] {"Finch"};
+			String[] from2 = new String[] {"Leslie"};
+			String[] to2 = new String[] {"Don Mills"};
+			
+			Double fare = f.calculateTotal(from1, to2);
+			assertEquals(5.50,fare,0.0);
+			
+		}
 }
