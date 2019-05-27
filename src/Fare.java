8096b3c0a13bbe9c837f914a588030cb471bfa41
navleensahi <navleenk1@gmail.com>
2019-05-27 12:06:02 -0400

    R1- GREEN

--- src/Fare.java
+++ src/Fare.java
@@ -5,7 +5,9 @@ public class Fare {
 		String[] from1 = new String[] {"Leslie"};
 		String[] to1 = new String[] {"Don Mills"};
 		
-		
+		if(from[0].contains("Leslie") && to[0].contains("Don Mills")) {
+			return 2.50;
+		}
 		return 0.0;
 	}
 	
